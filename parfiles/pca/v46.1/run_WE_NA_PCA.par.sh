9925685e40a645eedbb8a7ede4c45221c8c6b45e
Harald Ringbauer <harald_ringbauer@hms.harvard.edu>
2021-07-07 19:24:19 -0400

    Update v48.2 PCA

---
+++ parfiles/pca/v46.1/run_WE_NA_PCA.par.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --partition=priority
+#SBATCH -t 20:00:00		# Time in HH:MM:SS
+#SBATCH -c 1                    # Number of cores requested
+#SBATCH -N 1                    # Ensure that all cores are on one machine (span[hosts=1])
+#SBATCH --mem=60G               # Memory total in GB (see also --mem-per-cpu)
+#SBATCH --output=./logs/%A_%a.out
+#SBATCH --error=./logs/%A_%a.err
+
+##### N&I NAGIC #####
+LD_LIBRARY_PATH=/opt/lsf/7.0/linux2.6-glibc2.3-x86_64/lib:/opt/nag/libC/lib:/usr/lib
+NAG_KUSARI_FILE=/opt/nag/nag.license
+LM_LICENSE_FILE=/opt/nag/license.dat
+
+module load gcc
+module load gsl/2.3
+module load openblas
+#module load R
+module load graphviz
+#module load matlab
+module load fftw
+
+PATH="$PATH:~np29/o2bin"
+PATH="$PATH:/n/groups/reich/iosif/sw/fs-2.0.7"
+PATH="$PATH:/n/groups/reich/iosif/sw/msdir/msdir"
+
+##### PARAMS #####
+TDIR="/n/scratch2/am483"
+PFILE="run_WE_NA_PCA.par"
+
+##### ACTION #####
+~np29/o2bin/smartpca -p ./$PFILE
