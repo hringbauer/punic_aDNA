8455cfa0df55443febc3d5704d645e6a8a4504e3
Harald Ringbauer <harald_ringbauer@hms.harvard.edu>
2021-08-03 08:27:38 -0400

    Update North African PCA: Sigmathreshold 9

---
+++ python/plot_qpadm.py
@@ -0,0 +1,481 @@
+#####
+# Scripts to plot qpAdm Plots
+#
+####
+
+import numpy as np
+import os  # For Saving to Folder
+import pandas as pd
+import matplotlib.pyplot as plt
+import socket
+import os as os
+import sys as sys
+import multiprocessing as mp
+import itertools as it
+from time import time
+
+def load_qp_adm(path, infeasible=False):
+    """Load, parse and return qp-Adm file.
+    Return: 
+    res: mxnx2 arryay: m: Nr subsets admixed pops, n: Nr 
+    p_vals: List of m p-Values
+    pops: List of Strings of analyzed populations. First is target, rest sources.
+    infeasible: Whether to return infeasible as result"""
+    pop_line, pop_line_end = -1, -1 # Where the populations are found   
+    res_begin, res_end = -1, -1
+    std_line = -1  # Where the standard Deviation will land
+    
+    # Iterate over everything and use the signals for start/stop
+    with open(path, "r") as f:
+        for i, line in enumerate(f):
+            if line =="left pops:\n":
+                pop_line = i+1 
+            if line=="right pops:\n":
+                pop_line_end = i-1 # There is an empty line before
+                
+            # Parse off everything to first space
+            s0 = line.split()
+            if len(s0)>=2:
+                #if s0[0] == "summ:":
+                #    res_begin= i+3
+                if s0[0] == "fixed" and s0[1]=="pat":
+                    res_begin = i+1
+                    
+                if s0[0] == "best" and s0[1]=="pat:":
+                    if res_end < 0: # Only take the first occurence
+                        res_end = i
+                        
+                elif s0[0] == "std." and s0[1]=="errors:":
+                    std_line = i
+    
+    ### Read out the results:
+    with open(path, "r") as f:
+        lines = f.readlines()
+        pops = lines[pop_line:pop_line_end]
+        pops = [p.rstrip() for p in pops]  # Chews off new line symbol
+
+        res = lines[res_begin:res_end]
+        stds = lines[std_line]
+    
+    # Post-process the important Lines:
+    res_t = np.array([s.split()[:len(pops)+4] for s in res]) # 8 is valid for 3 pops!
+    res = res_t[:,5:].astype("float")
+    p_vals = res_t[:,4].astype("float")
+    stds = np.array(stds.split())[2:].astype("float")  # Extract the Standard Deviations for the first line
+            
+    # Read out estimates and p-Value
+    assert(len(p_vals)==len(res)) # Sanity Check
+    return res, p_vals, pops, stds
+
+def load_pvals(path):
+    """Load and parse qpAdm log file for p-Value.
+    Return: 
+    p-val:  p-Value
+    pops: [Target, Source]"""
+    
+    pop_line, pop_line_end = -1, -1 # Where the populations are found   
+    p_val_line = -1
+    
+    # Iterate over everything and use the signals for start/stop
+    with open(path, "r") as f:
+        for i, line in enumerate(f):
+            if line =="left pops:\n":
+                pop_line = i+1 
+            if line=="right pops:\n":
+                pop_line_end = i-1 # There is an empty line before
+                
+            if line=="codimension 1\n":
+                p_val_line = i+2
+    
+    ### Read out the results:
+    with open(path, "r") as f:
+        lines = f.readlines()
+        pops = lines[pop_line:pop_line_end]
+        pops = [p.rstrip() for p in pops]  # Chews off new line symbol
+
+        pval = lines[p_val_line].split()[7]
+          
+    return float(pval), pops   
+
+
+def fig_admix(axes=[], res=[], p_vals=[], target_pops=[], 
+              source_pops=[], figsize=(12,8), 
+              save_path="", lw=2,
+              pval_lim=[1e-6,1.0], stds=[], 
+              l_pos = (1, 0.5), fs = 12, bw = 0.85, 
+              c=[], ec = "white", show=False,
+              height_ratios=[1, 8], xlabels=[],
+              rotation=90, legend=True):
+    """Make a Figure of the admixture coefficients.
+    axes: 2 axis where to plot Fractions and p-Values onto
+    res: nxk Array of Results
+    p_vals: Array of p-Vals [n]
+    target_pops: String Array of Target Pops [n]
+    source_pops: String Array of Source Pops [k]
+    stds: Standard Errors. If none are given do not plot them
+    l_pos: Where to place the Legend.
+    lw: Linewidth of bar"""
+    # Create Pandas Dataframe for Plot
+    #pd.DataFrame(data=target_pops, index=r, columns=source_pops)
+    
+    ### Define Axes if not given
+    if len(axes)==0:
+        fig, axes = plt.subplots(nrows=2, ncols=1,
+                                 gridspec_kw={'height_ratios': height_ratios}, 
+                                 figsize=figsize)
+    ax0, ax1 = axes
+    
+    # Plot Parameters
+    r = np.arange(len(target_pops))
+    barWidth = bw
+    if len(c)==0:
+        c=["DeepPink", "MediumBlue", "purple", "yellowgreen", "gold"]
+    
+    for i, s in reversed(list(enumerate(source_pops))): # From last to first (For Legend)
+        s = source_pops[i]
+        b = np.sum(res[:,:i], axis=1)
+        ax1.bar(r, res[:,i], bottom=b, color=c[i], edgecolor=ec, width=barWidth, label=s, alpha=0.9)
+        
+        if len(stds)>0: # Plot some standard deviations.
+            ax1.errorbar(r, b+res[:,i], yerr=stds[:,i], fmt='none', linewidth=lw, color="k")
+    
+    ### Add the Model not viable bars:
+    su = np.sum(res, axis=1)
+    sum0ids = np.where(su==0)[0]
+    
+    for i in sum0ids:
+        ax1.bar(i, 1, color="lightgray", edgecolor='white', width=barWidth, alpha=0.9)
+     
+    ################
+    # Custom x axis
+    ax1.set_xticks(r)
+    
+    if len(xlabels)==0:
+        xlabels = target_pops
+    ax1.set_xticklabels(xlabels, fontsize=fs, rotation=rotation)
+
+    # Add a legend
+    #l1 = ax1.legend(loc=l_pos, bbox_to_anchor=(1, 0.5), ncol=1, fontsize=fs)
+    if legend:
+        l1 = ax1.legend(bbox_to_anchor=l_pos, ncol=1, fontsize=fs)
+        l1.set_title('Sources: ', prop={'size':fs})
+    
+    ax1.set_ylabel("Fraction Ancestry", fontsize=fs*1.5)
+    ax1.set_ylim([0,1])
+    ax1.set_xlim([-0.6, len(target_pops)-0.4])
+    
+    ax0.set_yscale("log")
+    ax0.set_ylim(pval_lim)
+    ax0.bar(r, p_vals, color="grey", width=barWidth, alpha=0.8, zorder=1)
+    ax0.axhline(y=0.05, color='r', zorder=0)
+    ax0.set_ylabel("p-Val", fontsize=fs)
+    ax0.set_xlim([-0.6,len(target_pops)-0.4])
+    ax0.set_xticks([])
+    
+    # Adjust position of subplots
+    #plt.subplots_adjust(left=0, bottom=0.05, right=None,
+    #            wspace=0, hspace=0.06)
+    
+    if len(save_path) > 0:
+        plt.savefig(save_path, bbox_inches = 'tight', pad_inches = 0)
+        print("Saved to %s" % save_path)
+        # Save the .csv of the Results
+    
+    if show:
+        plt.show()
+
+def create_admix_df(source_pops, admix_coeffs, stds, p_vals):
+    """Create Dataframe with Admixture Proportions and Standard Errors.
+    Assume all input is as Numpy Array (otherwise indexing errors!!)"""
+    n = len(source_pops[0,:])  # Get Number Sources +1
+    df = pd.DataFrame({"target":source_pops[:,0], "p-Value":p_vals})
+    
+    for i in range(1,n):
+        df[f"Source_{i}"] = source_pops[:,i]
+        
+    for i in range(1,n):
+        df[f"Fraction_{i}"] = admix_coeffs[:,i-1]
+        
+    for i in range(1,n):
+        df[f"STD_{i}"] = stds[:,i-1]
+    return df
+    
+def give_admix0(res, minval=-1e-4):
+    """Return the Admixture Coefficients of the first Model.
+    Only return 0 if feasible, else nana"""
+    
+    feasible = np.min(res[0])>minval  # Check if feasible
+    if feasible == 0:
+        return np.nan  # No Feasible Model
+    return 0
+    
+def give_admix_index(res):
+    """Return index of first viable admixture result.
+    res: nxk array. n...Nr of all subsets, k...Nr of Source Pops"""    
+    for i, ls in enumerate(res):
+        if np.min(ls)>-1e-4:
+            return i
+    print("Warning: No valid admixture found!!")       
+    return np.nan # Default Return
+
+def give_admix_index_best(res, pvals, minval=-1e-4):
+    """Return index of best viable admixture result.
+    res: nxk array. n...Nr of all subsets, k...Nr of Source Pops"""  
+    feasible = np.min(res, axis=1)>minval  # Extract all feasible results
+    pvals_okay = feasible * pvals  # Set bad ones to 0.
+    
+    if np.max(pvals_okay)==0:
+        print("Warning: No valid admixture found!!")
+        return np.nan
+    
+    i = np.argmax(pvals_okay) # The Index with the Maximum Value
+    return i
+    
+def sci_notation(num, decimal_digits=1, precision=None, exponent=None):
+    """
+    Returns a string representation of the scientific
+    notation of the given number formatted for use with
+    LaTeX or Mathtext, with specified number of significant
+    decimal digits and precision (number of decimal digits
+    to show). The exponent to be used can also be specified
+    explicitly.
+    """
+    if not exponent:
+        exponent = int(np.floor(np.log10(abs(num))))
+    coeff = round(num / float(10**exponent), decimal_digits)
+    if not precision:
+        precision = decimal_digits
+
+    return "${0:.{2}f}\cdot10^{{{1:d}}}$".format(coeff, exponent, precision)
+
+
+def create_latex_lines(source_pops, admix_coeffs, stds, p_vals, na = "-", rp = "A12"):
+    """Print a Line for Latex Table in the main Text. 
+    Script to speed things up.
+    source_pops, nx(j+1) Array, j...NR of Sources (<=3)
+    admix_coeffs, stds: nxj Array
+    p_vals: nx1 Array
+    na: Character for Missing Data
+    rp: String for right population"""
+    out =""
+    
+    for i in range(len(source_pops)): # Iterate over every line
+        ls = [na for _ in range(12)] # Create empty vector with na Symbol
+        
+        assert(len(source_pops[i])<=4) # At most 4 sources
+        
+        for j, s in enumerate(source_pops[i]): # Fill in the Source Populations
+            ls[j] = s   
+            
+        ls[4] = rp        # Right Pop
+        
+        p = p_vals[i]
+        
+        if p==0.0:
+            ls[5] = "0" 
+        elif p>=0.05:
+            ls[5] = "\\textbf{" +  str(np.around(p, 3)) + "}" # Make bold
+            
+        elif p>= 0.01:
+            ls[5] = np.around(p, 3) # Round to three Digits            
+        elif p<0.01: 
+            ls[5] = sci_notation(p, decimal_digits=1)  # Do proper formatting      
+        
+        for j, x in enumerate(admix_coeffs[i]): # Fill in the admixture fractions
+            ls[6+j] = "{:.3f}".format(x) 
+            
+        for j, x in enumerate(stds[i]): # Fill in the uncertainty of admixture fractions
+            ls[9+j] = "{:.3f}".format(x)        
+        
+        s = " & ".join(str(x) for x in ls) # Convert to Strings and Join
+        s=s.replace("_", "-") # Replace tricky underscore symbols (for Latex tables)
+        print(s + "\\\\") # Add two backslashes
+        out += (s + "\n") # Do next Line
+        
+    return out # Return the full text
+
+def plot_qpadm(dir_path, test_pops, save_path="", l_pos = (0.4, 1.15), 
+               best=True, figsize=(12,8), bw = 0.85, lw=2,
+               height_ratios = [1,8], xlabels=[],
+               c=[], ec="white", fs=10, pval_lim = [1e-3,1],
+               latex=False, dataframe=False):
+    """Do 3 Way Admixtures of Sardinia
+    best: Whether to use highest p-Val Submodel: 0 use the first, 
+    True use the best p-Value, 
+    else use the old (first feasible)"""
+    admix_coeffs = []
+    p_vals = []
+    pops_t = [] 
+    stds = []
+    pr = False # Print the statistics
+
+    #for filename in os.listdir(dir_path):
+    for f in test_pops:
+        full_path = dir_path + str(f) + ".log" # Other "outputMycSicily_"
+        res, p_val_ls, pops, std = load_qp_adm(full_path)
+
+        pops_t.append(pops[0])
+        
+        if best == 0:
+            i = give_admix0(res)   # Give the first result (if feasible)
+            
+        elif best == True:
+            i = give_admix_index_best(res, p_val_ls) # Give the best feasible p-Value
+        
+        else:
+            i = give_admix_index(res)
+            
+        #### Handle Not fitting Models  
+        if np.isnan(i): # In Case no valid admixture:
+            new_res = np.zeros(np.shape(res)[1]) # Not Plot anything
+            std = np.zeros(np.shape(res)[1])     # Not Plot anything
+            new_p = -1 # For Sorting
+        
+        else:
+            new_res = res[i,:]
+            new_p = p_val_ls[i]
+        
+        admix_coeffs.append(new_res)
+        p_vals.append(new_p)
+        stds.append(std)
+
+        if pr == True:
+            print("Population: %s" % f)
+            print(res[i,:])
+            print(std)
+            print(p_val_ls[i])
+
+    admix_coeffs, stds = np.array(admix_coeffs), np.array(stds)
+    source_pops = pops[1:]
+    
+    print(pops_t)
+    
+    fig_admix(res=admix_coeffs, 
+              p_vals=p_vals, 
+              target_pops=pops_t, 
+              source_pops=source_pops, 
+              pval_lim=pval_lim, 
+              stds=stds, save_path = save_path, 
+              l_pos = l_pos, lw=lw, xlabels=xlabels,
+              height_ratios=height_ratios,
+              fs=fs, figsize=figsize, 
+              bw = bw, c=c, ec = ec)
+    
+    if latex:
+        source_pops = [([t] + pops[1:]) for t in pops_t]
+        create_latex_lines(source_pops, admix_coeffs, stds, 
+                           p_vals, na = "-", rp = "A15")
+        
+    if dataframe:
+        source_pops = np.array([([t] + pops[1:]) for t in pops_t])
+        df = create_admix_df(source_pops, admix_coeffs, stds, p_vals)
+        return(df)
+    
+    
+def plot_qpadm_split(dir_path, test_pops=[[]], 
+                     save_path="", 
+                     best=True, figsize=(12,8),
+                     labels=[],
+                     bw = 0.85, lw=2,
+                     c=[], ec="white", fs=10, 
+                     height_ratios=[1, 10], wspace=0.05, hspace=0.05,
+                     pval_lim = [1e-3, 1], pr=False,
+                     legend=True, l_pos=[]):
+    """Do 3 Way Admixtures of Sardinia
+    best: Whether to use highest p-Val Submodel: 0 use the first, True use the best p-Value, 
+    else use the old (first feasible)
+    test_pops: List of lists: Will be split up into subdataframes
+    pr: whether to print output"""
+    
+    ### Prepare the Plots
+    width_ratios = [len(ls) for ls in test_pops]
+    
+    fig = plt.figure(figsize=figsize)
+    
+    gs = gridspec.GridSpec(2, len(test_pops), 
+                           width_ratios = width_ratios, 
+                           height_ratios = height_ratios, 
+                           figure = fig)
+    
+    gs.update(wspace=wspace, hspace=hspace) # set the spacing between axes
+    
+    for j, ls in enumerate(test_pops):        
+        # make empty vectors for loading and plotting
+        admix_coeffs = []
+        p_vals = []
+        pops_t = [] 
+        stds = []
+    
+        for f in ls:
+            full_path = dir_path + str(f) + ".log" # Other "outputMycSicily_"
+            res, p_val_ls, pops, std = load_qp_adm(full_path)
+
+            pops_t.append(pops[0])
+
+            if best == 0:
+                i = give_admix0(res)   # Give the first result (if feasible)
+
+            elif best == True:
+                i = give_admix_index_best(res, p_val_ls) # Give the best feasible p-Value
+
+            else:
+                i = give_admix_index(res)
+
+            #### Handle Not fitting Models  
+            if np.isnan(i): # In Case no valid admixture:
+                new_res = np.zeros(np.shape(res)[1]) # Not Plot anything
+                std = np.zeros(np.shape(res)[1])     # Not Plot anything
+                new_p = -1 # For Sorting
+
+            else:
+                new_res = res[i,:]
+                new_p = p_val_ls[i]
+
+            admix_coeffs.append(new_res)
+            p_vals.append(new_p)
+            stds.append(std)
+
+            if pr == True:
+                print("Population: %s" % f)
+                print(res[i,:])
+                print(std)
+                print(p_val_ls[i])
+        
+        ### Do the actual Plot
+        ax_adm = fig.add_subplot(gs[0, j])
+        ax_p = fig.add_subplot(gs[1, j])
+        
+        #ax_adm = plt.subplot(gs[0, i])
+        #ax_p = plt.subplot(gs[1, i])
+        
+        admix_coeffs, stds = np.array(admix_coeffs), np.array(stds)
+        source_pops = pops[1:]
+        if j>0:
+            legend=False
+            
+        fig_admix(axes=[ax_adm, ax_p], res=admix_coeffs, 
+                  p_vals=p_vals, 
+                  target_pops=pops_t, 
+                  source_pops=source_pops, 
+                  pval_lim=pval_lim, 
+                  stds=stds, save_path = "", 
+                  lw=lw, 
+                  fs=fs, figsize=figsize, 
+                  bw = bw, c=c, ec = ec, show=False,
+                  legend=legend, l_pos = l_pos)
+        
+        ### Turn off the Labels for all but first plot
+        if j>0:
+            for ax in [ax_p, ax_adm]:
+                ax.set_yticklabels([])
+                ax.set_ylabel("")
+                
+        if len(labels)>0:
+                ax_adm.set_title(labels[j], fontsize=fs)
+            
+    if len(save_path) > 0:
+        plt.savefig(save_path, bbox_inches = 'tight', 
+                    pad_inches = 0)
+        print("Saved to %s" % save_path)
+        # Save the .csv of the Results
+    plt.show()
---
+++ python/run_qpadm.py
@@ -0,0 +1,245 @@
+#####
+# Scripts to run qpAdm from Python
+# Includes Wrappers for qpAdm
+# and functins to generate modified .ind files
+####
+
+import numpy as np
+import os  # For Saving to Folder
+import pandas as pd
+import matplotlib.pyplot as plt
+import socket
+import os as os
+import sys as sys
+import multiprocessing as mp
+import itertools as it
+from time import time
+
+def qpAdm_run(leftpops, rightpops, output_file, 
+              input_folder = "./eigenstrat/combined/", 
+              input_file="punic.v44",
+              par_file_folder = "./parfiles/", 
+              input_ind_suff="_mod1", 
+              output_folder ="./output/qpAdm/", 
+              path_bin_qpAdm = "/n/groups/reich/hringbauer/o2bin/qpAdm",
+              all_snps=False):
+    """Run qpAdm. Write temporary parfile and run the analysis
+    leftpops: List of left populations
+    rightpops: List of right populations
+    input_folder: Where to find the input files
+    par_file_folder: Folder of the parameterfile
+    input_file: The name of the input file
+    input_ind_suff: Suffix of .ind file. To allow modified populations
+    input_ind: name of the input .ind file. If given - write over default"""
+    #print(os.getcwd())
+    parfile_path = par_file_folder + "parfile." + output_file
+    left_path = par_file_folder + "left." + output_file
+    right_path = par_file_folder + "right." + output_file
+    
+    ### Create the parfile:
+    with open(parfile_path, 'w') as f:
+        f.write("%s\n" % ("DIR: " + input_folder))
+        f.write("%s\n" % ("S1: " + input_file))
+        
+        indline = "indivname: DIR/S1" + input_ind_suff + ".ind"
+        f.write("%s\n" % indline)
+        f.write("%s\n" % "snpname: DIR/S1.snp")
+        f.write("%s\n" % "genotypename: DIR/S1.geno")
+        f.write("%s\n" % ("popleft: " + left_path))
+        f.write("%s\n" % ("popright: " + right_path))
+        f.write("%s\n" % "details: YES")   
+        if all_snps==True:
+            f.write("%s\n" % "allsnps: YES")
+    
+    ### Write leftpops rightpops:       
+    with open(left_path, 'w') as f:
+        f.write("\n".join(leftpops))
+        
+    with open(right_path, 'w') as f:
+        f.write("\n".join(rightpops))
+    
+    output_path = output_folder + output_file + ".log"
+    command = f"{path_bin_qpAdm} -p {parfile_path} > {output_path}"
+    #print(f"Running command: \n{command}")
+    
+    ### Run qpAdm
+    start = time()
+    #!$path_bin_qpAdm -p $parfile_path > $output_path
+    os.system(command)
+    end = time()
+    
+    print("Runtime: %2f" % (end - start))
+    return 0
+
+############################################
+### Identify Individuals to run
+
+def load_iids_from_indfile(path_ind, string, 
+                           col="clst", col_iid="iid"):
+    """Load IIDs from Ind File
+    Return List of IIDs"""
+    df_ind = pd.read_csv(path_ind, delim_whitespace=True, header=None)
+    df_ind.columns=["iid", "sex", "clst"]
+    idx = df_ind[col].str.contains(string)
+    ls = df_ind[idx][col_iid].values
+    return ls
+
+def remove_individuals(df, remove_list=["_d"], iid_col="iid"):
+    """Remove indivdiuals from PCA dataframe"""
+    idx = df[iid_col].str.contains("|".join(remove_list))
+    df = df[~idx].copy()
+    print(f"Filtering to {len(df)}/{len(idx)}")
+    return df
+
+def get_sub_pops(df, pop_list=[], pop_col="clst"):
+    """Get Sub Populations"""
+    idx = df[pop_col].str.contains("|".join(pop_list))
+    df = df[idx].copy()
+    print(f"Found: {len(df)}/{len(idx)}")
+    return df
+
+def get_sub_pops_exact(df, pop_list=[], pop_col="clst"):
+    """Get Sub Populations"""
+    idx = df[pop_col].isin(pop_list)
+    df = df[idx].copy()
+    print(f"Found: {len(df)}/{len(idx)}")
+    return df
+
+def get_sub_iid(df, pops=[""], iid_col="iid"):
+    """Remove indivdiuals from PCA dataframe"""
+    idx = df[iid_col].str.contains("|".join(pops))
+    df = df[idx].copy()
+    print(f"Found: {len(df)}/{len(idx)}")
+    return df
+
+
+################################################
+### Modify Ind Files
+
+################################################################
+### To overwrite .ind Files
+
+def overwrite_ind_df(df, string, col="clst", 
+                     output=False, overwrite="", iids=False):
+    """Overwrite Individual Dataframe where col
+    contains string. Return modified dataframe (Copy)
+    where overwrite is the new Cluster ID
+    iids: Overwrite with IIDs if True!"""
+    idx = df[col].str.contains(string)
+    
+    if np.sum(idx)==0:
+        if output: 
+            print("No Indivdiuals found")
+        return
+    
+    if output:
+        print(f"Found {np.sum(idx)} Matches")
+        print(df[idx][col].value_counts())
+    
+    ### Actually  overwrite the Column
+    if len(overwrite)>0:
+        df.loc[idx, col] = overwrite
+        if output: 
+            print(f"{np.sum(idx)} Overwritten!")
+            
+    if iids:
+        df.loc[idx, col] = df.loc[idx, "iid"] 
+        
+        
+### Overwrite Individual IIDs
+def modifiy_iid_files(df_ind, pops_overwrite, 
+                      pops_overwrite12=[], ind_modified=""):
+    """Modify .ind file. Overwrite individuals from pops_overwrite (list)
+    with their individuals labels. 
+    df_int: Dataframe from Individuals.
+    pops_overwrite12: [[pop1,pop2]] list (nx2). Overwrites ALL string matches
+    for pop1 (contain) with pop2"""
+    
+    ### Overwrite with other Label
+    for pop1,pop2 in pops_overwrite12:
+        overwrite_ind_df(df_ind, pop1, overwrite=pop2)
+        
+    ### Overwrite with individual IIds
+    for pop in pops_overwrite:
+        overwrite_ind_df(df_ind, pop, 
+                     iids=True, output=True)
+    
+    ### Save here
+    df_ind.to_csv(ind_modified, sep=" ", index=None, header=False)
+    print(f"Saved {len(df_ind)} Individuals to {ind_modified}")
+    
+def set_iids_to_label(df_ind, iids_overwrite, label_new="",
+                    pops_overwrite12=[], savepath=""):
+    """Modify .ind file. Overwrite individuals from pops_overwrite (list)
+    with their individuals labels. 
+    df_ind: Dataframe from Individuals in .ind file
+    iids_overwrite: List of iids to overwrite
+    label_new: New label. If empty, use individual iids
+    pops_overwrite12: [[pop1,pop2]] list (nx2). Overwrites ALL string matches
+    for pop1 (contain) with pop2"""
+    
+    ### Overwrite with other Label
+    idx = df_ind["iid"].isin(iids_overwrite)
+    print(f"Overwriting {np.sum(idx)} Individuals")
+    
+    if len(label_new)==0:
+        df_ind.loc[idx, "clst"] = df_ind.loc[idx, "iid"]  ### Use indiviual Labels
+    else:
+        df_ind.loc[idx, "clst"] = label_new               ### Use new group label
+        
+    ### Overwrite with other Label
+    for pop1,pop2 in pops_overwrite12:
+        overwrite_ind_df(df_ind, pop1, overwrite=pop2)
+    
+    ### Save here
+    if len(savepath)>0:
+        df_ind.to_csv(savepath, sep=" ", index=None, header=False)
+        print(f"Saved {len(df_ind)} Individuals to {savepath}")
+        
+        
+def load_individuals_filetered(
+                            path_anno = "/n/groups/reich/hringbauer/Data/v42.3.anno.csv",
+                            col="clst", col_iid="iid",
+                            min_snps_cov=50000,
+                            snp_cov_col="n_cov_snp",
+                            master_id_col="Master ID",
+                            ):
+    """Filter List of Individuals against meta,
+    using minimal Nr of SNPs and unique IDs"""
+
+    df_all = pd.read_csv(path_anno)
+
+    ### Keep only the best coerage Indivdual
+    df_all = df_all.sort_values(by=snp_cov_col, ascending=False)
+    df_all = df_all.drop_duplicates(subset=master_id_col)
+    
+    ### Filter to min Nr of SNPs
+    df_all = df_all[df_all[snp_cov_col]>=min_snps_cov]
+    
+    ### 
+    df_all = df_all
+    return df_all["iid"].values
+
+def get_meta_ind_table(path_ind="./eigenstrat/combined/punic.v49.0.ind",
+                       path_anno="/n/groups/reich/hringbauer/Data/v49.0.anno.csv",
+                       min_snp=30000):
+    """Load .ind file, extract all individuals > min_snp SNPs
+    and merge with .anno File"""
+    df = pd.read_csv(path_ind, delim_whitespace=True, header=None)
+    df.columns=["iid", "sex", "clst"]
+    df = remove_individuals(df, remove_list=["_d"])
+    print(f"Loaded {len(df)} Individuals")
+
+    df_meta = pd.read_csv(path_anno, sep=",")
+    df2 = df_meta[["iid", "Master ID", "loc", "n_cov_snp", "mean_cov", "sex"]]
+    df = pd.merge(df, df2, on="iid", how="left")
+    idx = (df["n_cov_snp"]<min_snp)
+    df = df[~idx]
+    df.loc[df["loc"].isnull(), "loc"]="not assigned"
+    print(f"Filtered to {len(df)} Individuals based on #SNP covered> {min_snp}")
+
+    df = df.sort_values(by="n_cov_snp", ascending=False)
+    dup = (df["Master ID"].duplicated() & ~df["Master ID"].isnull())
+    df = df[~dup].copy().reset_index(drop=True)
+    print(f"Filtered to {len(df)} Individuals based on duplicates.")
+    return df
